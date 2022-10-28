a2c6d104448579bd05d331418d575e8528049b63
FinalRestartOfCoding <finalrestartofcoding@gmail.com>
2022-10-28 16:29:52 +0530

    Updated Comments

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+        // Initializing a variable via an expression of values.
         int myFirstNumber = (10 + 5) + (2 * 10);
 
         // Initializing a second variable via value.
         int mySecondNumber = 12;
 
-        // Initializing a third variable via variable.
+        // Initializing a third variable via an expression of variable and value.
         int myThirdNumber = myFirstNumber * 2;
 
-        // Using variables in an expression.
+        // Initializing a fourth variable via an expression of variables.
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
         // Prints the value stored in variable myTotal.
